3fae0f5fa90575f0530f33e9bca058ec902adb16
faii02032541 <50540147+faii02032541@users.noreply.github.com>
2020-03-11 19:51:42 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'H5JuvLNgnEjyFHVl4z3jvmnyuN+y1x17TqsSicZaPY5I7hJNFRN26k9B+1o9suLpMD2HjG2SSZRzOU2fqd752boiW15XfMhf0yQ4frJigw/KQ34g98ybaUMeFItavBeDZIJSrM7vkF2OHT/D7pZxvAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
